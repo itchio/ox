74ec64ee68710fe5f4d9504c9eb71937ee811054
Amos Wenger <amoswenger@gmail.com>
2019-07-05 19:09:34 +0200

    Introduce linox, SupportsUnprivilegedCloneNewUser

---
+++ linox/clone_newuser_linux.go
@@ -0,0 +1,22 @@
+package linox
+
+import (
+	"os/exec"
+	"syscall"
+)
+
+// SupportsUnprivilegedCloneNewuser returns true if
+// the Linux kernel allows unprivileged users to call the clone()
+// syscall with `CLONE_NEWUSER`.
+// It is useful, for example to establish whether the Electron 5.0+ suid sandbox
+// can be used, or if it needs to be disabled.
+// cf. https://github.com/electron/electron/issues/17972
+func SupportsUnprivilegedCloneNewuser() bool {
+	cmd := exec.Command("/bin/true")
+	cmd.SysProcAttr = &syscall.SysProcAttr{}
+	cmd.SysProcAttr.Cloneflags = syscall.CLONE_NEWUSER
+	if err := cmd.Run(); err != nil {
+		return false
+	}
+	return true
+}
